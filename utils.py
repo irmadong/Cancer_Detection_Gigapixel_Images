30b7b60d749075c421d57d370eb73dba3fc01eba
irmadong <jing.dong@richmond.edu>
2022-12-22 08:03:38 +0000

    finetuning the model ing

--- utils.py
+++ utils.py
@@ -14,8 +14,12 @@ import tensorflow as tf
 from keras.models import Model, load_model, Sequential
 from keras.layers import Dense,Flatten, Dropout, Conv2D, BatchNormalization, Activation, MaxPooling2D, concatenate
 from keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.applications.resnet import ResNet50
 from keras.optimizers import RMSprop
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+# import tensorflow.keras.applications.resnet50
+# from keras.applications.resnet50 import ResNet50
+
 
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -52,10 +56,13 @@ def apply_mask(im, mask, color=(255,0,0)):
     for x,y in mask: masked[x][y] = color
     return masked
 
-def check_center_region(patch, center_size):
+def check_center_region(patch, center_size = 128, threshold = 0.0):
     start = int(299//2)
     mid = int(center_size //2)
-    return np.sum(patch[start-mid: start + mid, start-mid: start + mid])>=1
+    percent = (np.sum(patch[start-mid: start + mid, start-mid: start + mid]))/(center_size*center_size)
+    center_region = np.array(patch[start-mid: start + mid, start-mid: start + mid])
+#     print(percent)
+    return percent > threshold
 
     
 
@@ -64,6 +71,7 @@ def get_windows(slide_path, tumor_mask_path, levels, stride=150, window_len=299)
     slide_windows1 = []
     slide_windows2 = []
     window_labels = []
+
 
     slide = open_slide(slide_path)
     tumor_mask = open_slide(tumor_mask_path)
@@ -93,13 +101,13 @@ def get_windows(slide_path, tumor_mask_path, levels, stride=150, window_len=299)
                                           level=level1,
                                           width=window_len,
                                           height=window_len)
+            
+            tumor_mask_image = tumor_mask_image[:,:,0]
 
             # calculate the percentage of tissue
             tissue_pixels = find_tissue_pixels(slide_image)
             #print(tissue_pixels)
-            percent_tissue = len(tissue_pixels) / float(slide_image.shape[0] * slide_image.shape[0] ) 
-        #print("curr_slide shape", curr_slide.shape[0])) 
-        #print("curr_slide shape", curr_slide.shape[0])
+            percent_tissue = len(tissue_pixels) / float(slide_image.shape[0] * slide_image.shape[0]) 
 
 
 
@@ -113,18 +121,82 @@ def get_windows(slide_path, tumor_mask_path, levels, stride=150, window_len=299)
                                                  width = window_len, 
                                                  height = window_len
                                                 ))
-                if check_center_region(slide_image, 128):
+                if check_center_region(tumor_mask_image, 128):
                     #if the center has tumor --> 1 
                     window_labels.append(1)
                 else:
                     window_labels.append(0)
 
-    #             if np.sum(tumor_mask_image[:,:,0]) / (window_len * window_len) >= threshold:
-    #                 window_labels.append(1)
-    #             else:
-    #                 window_labels.append(0)
 
     return slide_windows1, slide_windows2, window_labels    
+
+
+# get windows on
+def get_test_windows(slide_path, tumor_mask_path, levels, stride=299, window_len=299):
+    slide_windows1 = []
+    slide_windows2 = []
+    window_labels = []
+    coord_1 = [] #todo: which coord? to level0 or itself? currently itself 
+    coord_2 = []
+
+
+    slide = open_slide(slide_path)
+    tumor_mask = open_slide(tumor_mask_path)
+    level1, level2 = levels[0], levels[1]
+
+    window_width = slide.level_dimensions[level1][0] - window_len + 1 
+    window_height = slide.level_dimensions[level1][1] - window_len + 1
+    downsample_factor1 = slide.level_downsamples[level1]
+    downsample_factor2 = slide.level_downsamples[level2]
+    
+    # sliding window
+    for w in range(0, window_height, stride):
+        for h in range(0, window_width, stride):
+            curr_coord = (int(w*downsample_factor1), int(h*downsample_factor1))
+            center_coord = (int((w+window_len//2)*downsample_factor1), int((h+window_len//2)*downsample_factor1))
+
+            slide_image = read_slide(slide,
+                                     x=curr_coord[0],
+                                     y=curr_coord[1],
+                                     level=level1,
+                                     width=window_len,
+                                     height=window_len)
+
+            tumor_mask_image = read_slide(tumor_mask,
+                                          x=curr_coord[0],
+                                          y=curr_coord[1],
+                                          level=level1,
+                                          width=window_len,
+                                          height=window_len)
+            
+            tumor_mask_image = tumor_mask_image[:,:,0]
+
+            # calculate the percentage of tissue
+            tissue_pixels = find_tissue_pixels(slide_image)
+            #print(tissue_pixels)
+            percent_tissue = len(tissue_pixels) / float(slide_image.shape[0] * slide_image.shape[0]) 
+
+            if percent_tissue >= 0.5 and np.mean(slide_image) > 0.2:
+                slide_windows1.append(slide_image)
+
+                slide_windows2.append(read_slide(slide,
+                                                 x = int(center_coord[0] //(2**(level2-level1)) - (window_len//2)*downsample_factor2), 
+                                                 y = int(center_coord[1] //(2**(level2-level1)) - (window_len//2)*downsample_factor2), 
+                                                 level = level2, 
+                                                 width = window_len, 
+                                                 height = window_len
+                                                ))
+                coord_1.append((int(w), int(h)))
+                coord_2.append((int(center_coord[0] //(2**(level2-level1))//downsample_factor2 -(window_len//2)),
+                                    int(center_coord[1] //(2**(level2-level1))//downsample_factor2 -(window_len//2))))
+                if check_center_region(tumor_mask_image, 128, 0.0):
+     
+                    window_labels.append(1)
+                else:
+                    window_labels.append(0)
+
+
+    return slide_windows1, slide_windows2, window_labels, coord_1, coord_2   
 
 
 def display_windows(images, row, col, x, y):
@@ -140,70 +212,71 @@ def display_windows(images, row, col, x, y):
       ax.axes.get_yaxis().set_visible(False)
 
 
-# def get_windows(slide_paths, tumor_mask_paths, level, stride = 150, window_len=299, threshold = 0.5):
-def generate_raw_patch(slide_paths, level, stride=150, window_len=299):
-    slide_windows = []
-    slide_coord = []
-    slide_level0_coord = []
-    #   contains_cancer = []
+# +
+# # def get_windows(slide_paths, tumor_mask_paths, level, stride = 150, window_len=299, threshold = 0.5):
+# def generate_raw_patch(slide_paths, level, stride=150, window_len=299):
+#     slide_windows = []
+#     slide_coord = []
+#     slide_level0_coord = []
+#     #   contains_cancer = []
 
-    #   for slide_path, tumor_mask_path in zip(slide_paths, tumor_mask_paths):
-    for slide_path in slide_paths:
-        slide = open_slide(slide_path)
-        #         tumor_mask = open_slide(tumor_mask_path)
+#     #   for slide_path, tumor_mask_path in zip(slide_paths, tumor_mask_paths):
+#     for slide_path in slide_paths:
+#         slide = open_slide(slide_path)
+#         #         tumor_mask = open_slide(tumor_mask_path)
 
-        window_width = slide.level_dimensions[level][0] - window_len + 1
-        window_height = slide.level_dimensions[level][1] - window_len + 1
-        downsample_factor = slide.level_downsamples[level]
+#         window_width = slide.level_dimensions[level][0] - window_len + 1
+#         window_height = slide.level_dimensions[level][1] - window_len + 1
+#         downsample_factor = slide.level_downsamples[level]
 
-        # sliding window
-        for w in range(0, window_width, stride):
-            for h in range(0, window_height, stride):
-                level0_coord = (int(w * downsample_factor), int(h * downsample_factor))
-                curr_coord = (w, h)
+#         # sliding window
+#         for w in range(0, window_width, stride):
+#             for h in range(0, window_height, stride):
+#                 level0_coord = (int(w * downsample_factor), int(h * downsample_factor))
+#                 curr_coord = (w, h)
 
-                curr_slide = read_slide(slide,
-                                        x=curr_coord[0],
-                                        y=curr_coord[1],
-                                        level=level,
-                                        width=window_len,
-                                        height=window_len)
-                slide_windows.append(curr_slide)
-                slide_coord.append(curr_coord)
-                slide_level0_coord.append(level0_coord)
+#                 curr_slide = read_slide(slide,
+#                                         x=curr_coord[0],
+#                                         y=curr_coord[1],
+#                                         level=level,
+#                                         width=window_len,
+#                                         height=window_len)
+#                 slide_windows.append(curr_slide)
+#                 slide_coord.append(curr_coord)
+#                 slide_level0_coord.append(level0_coord)
 
-    return slide_windows, slide_coord, slide_level0_coord
+#     return slide_windows, slide_coord, slide_level0_coord
 
-
-def make_prediction(model, slide_windows, slide_coord, wid, height, window_len = 299,
-                    stride = 150):
-    #maybe stride not needed?
-    #todo: do we need level0 coord? 
+# +
+# def make_prediction(model, slide_windows, slide_coord, wid, height, window_len = 299,
+#                     stride = 150):
+#     #maybe stride not needed?
+#     #todo: do we need level0 coord? 
     
-    final_output = np.zeros([wid, height])#todo: which dimension? 
-    #turn into batchsize 
+#     final_output = np.zeros([wid, height])#todo: which dimension? 
+#     #turn into batchsize 
         
-    fill_1 = np.ones([window_len, window_len])
-    fill_0 = np.zeros([window_len, window_len])
+#     fill_1 = np.ones([window_len, window_len])
+#     fill_0 = np.zeros([window_len, window_len])
     
-    for i in range(0, len(slide_windows), 100):
-        temp = slide_windows[i:i+100]
-        temp_coord = slide_coord[i:i+100]
-        temp = np.array(temp)
+#     for i in range(0, len(slide_windows), 100):
+#         temp = slide_windows[i:i+100]
+#         temp_coord = slide_coord[i:i+100]
+#         temp = np.array(temp)
     
-        pred = model.predict(temp)
-        pred = np.argmax(pred, axis=1) 
-        for i in range(len(temp_coord)):
-            x_left = slide_coord[i][0] #to do :check is it correct or need to swap? 
-            y_left = slide_coord[i][1]
-            if pred[i] == 1:
-                final_output[x_left: x_left + window_len, y_left : y_left + window_len] = fill_1
+#         pred = model.predict(temp)
+#         pred = np.argmax(pred, axis=1) 
+#         for i in range(len(temp_coord)):
+#             x_left = slide_coord[i][0] #to do :check is it correct or need to swap? 
+#             y_left = slide_coord[i][1]
+#             if pred[i] == 1:
+#                 final_output[x_left: x_left + window_len, y_left : y_left + window_len] = fill_1
 
-            #print("current coord is ",slide_coord[i] )
-#         else:
-#              final_output[x_left: x_left + window_len, y_left : y_left + window_len] = fill_0
-    return final_output
-
+#             #print("current coord is ",slide_coord[i] )
+# #         else:
+# #              final_output[x_left: x_left + window_len, y_left : y_left + window_len] = fill_0
+#     return final_output
+# -
 
 def shuffle_data(arr,arr2, label):
     w_tumor = [arr[i] for i in range(len(arr)) if label[i]==1]
@@ -212,14 +285,62 @@ def shuffle_data(arr,arr2, label):
     wo_tumor2 = [arr2[i] for i in range(len(arr2)) if label[i]==0]
     
     
-    shuffle_idx = np.arange(len(w_tumor))
+    shuffle_idx = np.arange(len(wo_tumor))
     np.random.shuffle(shuffle_idx)
     
-    w_tumor = [w_tumor[shuffle_idx[i]] for i in shuffle_idx[:len(wo_tumor)]]
-    w_tumor2 = [w_tumor2[shuffle_idx[i]] for i in shuffle_idx[:len(wo_tumor)]]
+    wo_tumor = [wo_tumor[shuffle_idx[i]] for i in shuffle_idx[:len(w_tumor)]]
+    wo_tumor2 = [wo_tumor2[shuffle_idx[i]] for i in shuffle_idx[:len(w_tumor)]]
     
     return w_tumor, w_tumor2, wo_tumor, wo_tumor2
 
+
+def cut_data(arr, arr2, label, size = 1250):
+    #seperate the data 
+    w_tumor = [arr[i] for i in range(len(arr)) if label[i]==1]
+    w_tumor2 = [arr2[i] for i in range(len(arr2)) if label[i]==1]
+    wo_tumor = [arr[i] for i in range(len(arr)) if label[i]==0]
+    wo_tumor2 = [arr2[i] for i in range(len(arr2)) if label[i]==0]
+    
+    #shuffle index  
+    shuffle_idx = np.arange(len(wo_tumor))
+    np.random.shuffle(shuffle_idx)
+    
+    wo_tumor = [wo_tumor[shuffle_idx[i]] for i in shuffle_idx[:size]]
+    wo_tumor2 = [wo_tumor2[shuffle_idx[i]] for i in shuffle_idx[:size]]
+    
+    w_tumor = [w_tumor[shuffle_idx[i]] for i in shuffle_idx[:size]]
+    w_tumor2 = [w_tumor2[shuffle_idx[i]] for i in shuffle_idx[:size]]
+    
+    
+    
+    
+    return w_tumor, w_tumor2, wo_tumor, wo_tumor2
+    
+
+# +
+# def data_reduction(train1, train2,  test1, test2, train_limit=2000, test_limit=500):
+#   len1 = len(test1)
+#   if len1 > train_limit:
+#     train1, test1 = shuffle(train1, test1, train_limit)
+  
+#   len2 = len(test2)
+#   if len2 > test_limit:
+#     train2, test2 = shuffle(train2, test2, test_limit)
+  
+#   return train1, train2, test1, test2
+
+# def shuffle(arr, label, limit):
+#   arr_pos = np.array([arr[i] for i in len(label) if label[i] == 1][:limit//2])
+#   arr_neg = np.array([arr[i] for i in len(label) if label[i] == 0][:limit - limit//2])
+#   arr = np.concatenate(arr_pos, arr_neg)
+#   label = np.concatenate(np.ones(len(arr_pos)), np.zeros(len(arr_neg)))
+#   shuffle_idx = np.arrange(len(arr))
+#   np.random.shuffle(shuffle_idx)
+#   arr = [arr[shuffle_idx[i]] for i in shuffle_idx]
+#   label = [label[shuffle_idx[i] for i in shuffle_idx]]
+
+#   return arr, label
+# -
 
 def preprocess(x):
 
@@ -231,6 +352,7 @@ def preprocess(x):
 
 
 def create_tf_dataset(X_train1, X_train2, y_train1):
+    
     train_ds_1 = tf.data.Dataset.from_tensor_slices(X_train1)
     #image_ds_1 = train_ds_1.map(preprocess)
 
@@ -245,7 +367,7 @@ def create_tf_dataset(X_train1, X_train2, y_train1):
     BATCH_SIZE = 16
     final_ds = image_ds.repeat()
     
-    final_ds = final_ds.shuffle(buffer_size=3000)
+    #final_ds = final_ds.shuffle(buffer_size=3000)
     final_ds = final_ds.batch(BATCH_SIZE)
     
     final_ds = final_ds.prefetch(1)
@@ -263,16 +385,20 @@ class multi_level_inception(tf.keras.Model):
         #conv_base.trainable = True
 
         self.model1 = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
+        
+            
         self.model1.trainable = True
         self.flatten1  = Flatten()
 
         self.model2 = InceptionV3(weights='imagenet', include_top=False, input_shape=(299, 299, 3))
         self.model2.trainable = True
+        for layer in self.model2.layers:
+            layer._name += str("_2")
         self.flatten2 = Flatten()
 
         self.concate_layer = Dense(128, activation='relu')
         self.dropout = Dropout(0.5)
-        self.last = Dense(1, activation='sigmoid')
+        self.last = Dense(1, activation='sigmoid')#todo check 1 or 2?
 
     def call(self, x):
         x1, x2= x[0], x[1]
@@ -290,3 +416,126 @@ class multi_level_inception(tf.keras.Model):
         x = self.last(x)
 
         return x
+
+
+def create_test_tfdataset(X_test1, X_test2):
+    #without label 
+    test_ds_1 = tf.data.Dataset.from_tensor_slices(X_test1)
+    #image_ds_1 = train_ds_1.map(preprocess)
+
+    test_ds_2 = tf.data.Dataset.from_tensor_slices(X_test2)
+    #image_ds_2 = train_ds_2.map(preprocess)
+    
+  
+
+    image_ds = tf.data.Dataset.zip(((test_ds_1,test_ds_2), ))
+
+
+    BATCH_SIZE = 4
+    #final_ds = image_ds.repeat()
+    
+    final_ds = image_ds.batch(BATCH_SIZE)
+    
+    final_ds = final_ds.prefetch(1)
+
+    return final_ds
+
+
+def make_prediction(model, slide_windows1, slide_windows2, window_labels, coord_1, heatmap):
+    #get label for the patch 
+    preds = []
+    for i in range(len(slide_windows1)):
+        testds = create_test_tfdataset(slide_windows1, slide_windows2)
+        print()
+        #pred = model.predict([preprocess(np.array(slide_windows1[i])), preprocess(np.array(slide_windows2[i]))])
+        pred = model.predict(testds)
+        #check prob or class maybe chance last dense layer into 1 
+        if i < 10:
+            print('before', pred)
+        pred = pred > 0.5
+        if i<10:
+            print("after", pred)
+        preds.append(pred)
+        w,h = coord_1[i]
+        if pred == 1:
+            midh = h + 299//2
+            midw = w+299//2
+            heatmap[int(midh - 64):(midh+64), (midw - 64): (midw + 64)] = 1
+        
+            
+    
+    return preds, heatmap
+
+# +
+# def make_prediction_batch(model, test_ds):
+#     #get label for the patch 
+#     preds = []
+#     for i in range(len(slide_windows1)):
+#         testds = create_test_tfdataset(slide_windows1, slide_windows2)
+#         print()
+#         #pred = model.predict([preprocess(np.array(slide_windows1[i])), preprocess(np.array(slide_windows2[i]))])
+#         pred = model.predict(testds)
+#         #check prob or class maybe chance last dense layer into 1 
+#         if i < 10:
+#             print('before', pred)
+#         pred = pred > 0.5
+#         if i<10:
+#             print("after", pred)
+#         preds.append(pred)
+#         w,h = coord_1[i]
+#         if pred == 1:
+#             midh = h + 299//2
+#             midw = w+299//2
+#             heatmap[int(midh - 64):(midh+64), (midw - 64): (midw + 64)] = 1
+        
+            
+    
+#     return preds, heatmap
+# -
+
+def generate_heat_map(model, pred, coord, heatmap, threshold = 0.5):
+    class_pred = []
+    for i in range(len(coord)):
+        
+        w,h = coord[i]
+        midh = h + 299//2
+        midw = w+299//2
+        if pred[i] > threshold: 
+            heatmap[int(midh - 64):(midh+64), (midw - 64): (midw + 64)] = 1 #check 
+            class_pred.append(1)
+        else:
+            #heatmap[int(midh - 64):(midh+64), (midw - 64): (midw + 64)] = 0 #check
+            class_pred.append(0)
+    return class_pred
+    
+
+
+def create_model():
+
+    model1 = InceptionV3(weights='imagenet', include_top=False, input_shape=(299,299, 3))
+    in1 = model1.input
+    out1 = model1.output
+
+    for layer in model1.layers:
+        layer.trainable = True
+
+    model2 = InceptionV3(weights='imagenet', include_top=False, input_shape=(299,299, 3))
+
+    for layer in model2.layers:
+        layer._name +=layer.name + str("_2")
+
+    in2 = model2.input
+    out2 = model2.output
+
+    for layer in model2.layers:
+        layer.trainable = True
+
+    concate= concatenate([out1, out2])
+
+    flat = Flatten()(concate)
+    dropout1 = Dropout(.5)(flat)
+    dense1 = Dense(128, activation='relu')(dropout1)
+    final_layer = Dense(1, activation='sigmoid')(dense1)
+    model = Model([in1, in2], final_layer)
+   
+    return model 
